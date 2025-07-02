53aa6e3128bbfdcce7a69324b337d5f54897633d
Kwangwon Min <kwangwon.min@gmail.com>
2025-07-02 15:44:10 +0900

    Add more about checkLength

--- simchecker.cpp
+++ simchecker.cpp
@@ -5,7 +5,19 @@ using std::string;
 class SimChecker {
 public:
 	int checkLength(string str1, string str2) {
+		// Same length, get max score
 		if (str1.length() == str2.length())
 			return 60;
+
+		int longer, shorter;
+		longer = std::max(str1.length(), str2.length());
+		shorter = std::min(str1.length(), str2.length());
+		int gap = longer - shorter;
+		
+		// gap is larger than two times, zero
+		if (longer >= shorter * 2)
+			return 0;
+		double score = 60 * (1 - static_cast<double>(gap) / shorter);
+		return score;
 	}
 };
--- simchecker_test.cpp
+++ simchecker_test.cpp
@@ -1,10 +1,28 @@
 #include "gmock/gmock.h"
 #include "simchecker.cpp"
 
-TEST(Simchecker, tc1) {
+TEST(Simchecker, sameLengthTest) {
 	SimChecker sc;
 	int score = sc.checkLength("asd", "dsa");
 	EXPECT_EQ(60, score);
+}
+
+TEST(Simchecker, zeroScoreTest) {
+	SimChecker sc;
+	int score = sc.checkLength("a", "bb");
+	EXPECT_EQ(0, score);
+}
+
+TEST(Simchecker, subScoreTest1) {
+	SimChecker sc;
+	int score = sc.checkLength("aaabb", "baa");
+	EXPECT_EQ(20, score);
+}
+
+TEST(Simchecker, subScoreTest2) {
+	SimChecker sc;
+	int score = sc.checkLength("aae", "aa");
+	EXPECT_EQ(30, score);
 }
 
 int main() {
