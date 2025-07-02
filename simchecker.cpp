e855812617d547576610d47ac1426682a8015db0
Kwangwon Min <kwangwon.min@gmail.com>
2025-07-02 16:02:39 +0900

    Refactor Checklength

--- simchecker.cpp
+++ simchecker.cpp
@@ -4,20 +4,24 @@ using std::string;
 
 class SimChecker {
 public:
-	int checkLength(string str1, string str2) {
-		// Same length, get max score
-		if (str1.length() == str2.length())
-			return 60;
 
-		int longer, shorter;
-		longer = std::max(str1.length(), str2.length());
-		shorter = std::min(str1.length(), str2.length());
+	const int MAX_SCORE_LENGTH = 60;
+
+	int checkLength(string str1, string str2) {
+		int longer = std::max(str1.length(), str2.length());
+		int shorter = std::min(str1.length(), str2.length());
+		// Same length, get max score
+		if (longer == shorter)
+			return MAX_SCORE_LENGTH;
+
+		return getScore(longer, shorter);
+	}
+private:
+	double getScore(int longer, int shorter) {
 		int gap = longer - shorter;
-		
-		// gap is larger than two times, zero
-		if (longer >= shorter * 2)
-			return 0;
 		double score = 60 * (1 - static_cast<double>(gap) / shorter);
+		if (score < 0) score = 0;
 		return score;
 	}
+
 };
